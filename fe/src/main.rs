e2defc805fc8ed11f63ae8eee3440d9c2c983ca8
ikey4u <pwnkeeper@gmail.com>
2024-04-20 23:24:34 +0800

    feat: add menu and router

--- fe/src/main.rs
+++ fe/src/main.rs
@@ -2,20 +2,128 @@
 
 use dioxus::prelude::*;
 
-const _STYLE: &str = manganis::mg!(file("public/tailwind.css"));
+#[derive(Routable, Clone)]
+#[rustfmt::skip]
+enum Route {
+    #[layout(NavBar)]
+        #[route("/")]
+        Think {},
+        #[route("/post/:name")]
+        ThinkPost { name: String },
 
-fn main() {
-    launch(App);
+        #[route("/spark")]
+        Spark {},
+
+        #[route("/about")]
+        About {},
+
+        #[route("/more")]
+        More {},
+    #[end_layout]
+
+    #[route("/:..route")]
+    PageNotFound {
+        route: Vec<String>,
+    },
 }
 
 pub fn App() -> Element {
-    rsx!(
+    rsx! { Router::<Route> {} }
+}
+
+#[component]
+fn NavBar() -> Element {
+    let current_route = use_route::<Route>().to_string();
+    rsx! {
         div {
-            class: "flex justify-center items-center h-screen",
-            p {
-                class: "text-center text-3xl text-blue-600 font-bold",
-                "Hello Dioxus!"
+            nav {
+                class: "bg-gray-100",
+                div {
+                    class: "flex items-center justify-center h-16",
+                    border: "1px solid green",
+                    ul {
+                        class: "flex space-x-4 font-bold",
+                        li {
+                            Link {
+                                color: if current_route == "/" { "red" } else { "" },
+                                to: Route::Think {},
+                                "THINK"
+                            }
+                        }
+                        li {
+                            Link {
+                                color: if current_route.starts_with("/spark") { "red" } else { "" },
+                                to: Route::Spark {},
+                                "SPARK" }
+                        }
+                        li {
+                            Link { to: Route::About {}, "ABOUT" }
+                        }
+                        li {
+                            Link { to: Route::More {}, "MORE." }
+                        }
+                    }
+                }
             }
         }
-    )
+        Outlet::<Route> {}
+    }
+}
+
+#[component]
+fn Spark() -> Element {
+    rsx! {
+        h1 { "SPARK TODO" }
+    }
+}
+
+#[component]
+fn Think() -> Element {
+    rsx! {
+        ul {
+            li {
+                Link {
+                    to: Route::ThinkPost {
+                        name: "Blog post 1".into(),
+                    },
+                    "Read the first blog post"
+                }
+            }
+            li {
+                Link {
+                    to: Route::ThinkPost {
+                        name: "Blog post 2".into(),
+                    },
+                    "Read the second blog post"
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn ThinkPost(name: String) -> Element {
+    rsx! { h2 { "Blog Post: {name}" } }
+}
+
+#[component]
+fn About() -> Element {
+    rsx!({ "ABOUT TODO" })
+}
+
+#[component]
+fn More() -> Element {
+    rsx!({ "MORE. TODO" })
+}
+
+#[component]
+fn PageNotFound(route: Vec<String>) -> Element {
+    rsx! {
+        h1 { "Page not found" }
+    }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    launch(App);
 }
